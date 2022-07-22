0fad9d1a9e9dc4e609923e650a42e3b930aa39e9
cchura94 <cchura.cpc@gmail.com>
2022-07-21 20:05:23 -0400

    categoria

--- src/controllers/categoria.controller.js
+++ src/controllers/categoria.controller.js
@@ -7,28 +7,24 @@ export const listaCategoria = async function(req, res){
 
 }
 
-export const guardarCategoria = function(req, res){
-    let id = req.params.id;
+export const guardarCategoria = async function(req, res){
+    // let id = req.params.id;
     let datos = req.body;
 
-    await models.Categoria.update(datos, {
-        where: {
-            id: id
-        }
-    });
+    await models.Categoria.create(datos);
     res.status(200).json({mensaje: "Categoria Actualizado", error: false})
 }
 
 
 
-export const mostrarCategoria = function(req, res){
+export const mostrarCategoria = async function(req, res){
     let id = req.params.id;
 
     let categoria = await models.Categoria.findByPk(id);
     res.status(200).json(categoria)
 }
 
-export const modificarCategoria = function(req, res){
+export const modificarCategoria = async function(req, res){
 
     let id = req.params.id;
     let datos = req.body;
